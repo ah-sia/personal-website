2a32a2f8e88605bbb78182895c505c7be1af6b0a
Aisha Pepino <150063867+ah-sia@users.noreply.github.com>
2024-06-11 21:57:27 -0400

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,29 @@
+﻿const formButton = document.querySelector("#formButton");
+formButton.addEventListener("click", getInput);
+
+function getInput() {
+    var sent = false;
+    var x = document.getElementById("feedback").value;
+    var f = document.getElementById("feedbackForm");
+    var t = document.getElementById("textArea");
+    if (x != null && x != "") {
+        formButton.removeEventListener("click", getInput);
+        let req = new XMLHttpRequest();
+        t.innerHTML = '<p>' + x + '</p>'
+        req.onreadystatechange = () => {
+            if (req.readyState == XMLHttpRequest.DONE && !sent) {
+                f.innerHTML = '<p>Thank you for your feedback!</p>';
+                sent = true;
+                var arr = JSON.parse(req.responseText)['record'];
+                req.open("PUT", "https://api.jsonbin.io/v3/b/<6570b9c212a5d37659a40357>", true);
+                req.setRequestHeader("Content-Type", "application/json");
+                req.setRequestHeader("X-ACCESS-Key", "<KEY>");
+                arr.push({ feedback: x });
+                req.send(JSON.stringify(arr));
+            }
+        };
+
+    req.open("GET", "https://api.jsonbin.io/v3/b/<6570b9c212a5d37659a40357>/latest", true);
+    req.setRequestHeader("X-ACCESS-Key", "<KEY>");
+    req.send();
+}
---
+++ first.js
@@ -0,0 +1,17 @@
+﻿window.transitionToPage = function (href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function () {
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.querySelector('body').style.opacity = 1
+});
+
+function sing(song) {
+    console.log(song);
+    console.log("TEEEEEE");
+}
+sing("LAAAAA");
+sing("twice!");
